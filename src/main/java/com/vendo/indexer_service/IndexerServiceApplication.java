c065e2bcd96f6172487d7d0861b0d911da7b4acf
HaaaamBurger <gemasclashes@gmail.com>
2026-04-29 18:13:39 +0300

    init project

---
+++ src/main/java/com/vendo/indexer_service/IndexerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vendo.indexer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndexerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndexerServiceApplication.class, args);
+	}
+
+}
