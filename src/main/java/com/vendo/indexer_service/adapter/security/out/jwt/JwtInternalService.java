fa3891f762ec794505fcc4d53a57b3439956e0a1
HaaaamBurger <gemasclashes@gmail.com>
2026-06-10 14:56:19 +0300

    kan-43: audience bug fix

--- src/main/java/com/vendo/indexer_service/adapter/security/out/jwt/JwtInternalService.java
+++ src/main/java/com/vendo/indexer_service/adapter/security/out/jwt/JwtInternalService.java
@@ -6,11 +6,11 @@ import com.vendo.indexer_service.adapter.security.out.jwt.props.JwtProperties;
 import com.vendo.security_lib.type.TokenClaim;
 import com.vendo.security_starter.jwt.JwtPayload;
 import com.vendo.security_starter.jwt.JwtService;
-import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
@@ -22,14 +22,10 @@ public class JwtInternalService implements InternalGenerationPort {
     public String generate(ServiceName audience) {
         JwtProperties.Internal internal = props.getInternal();
 
-        Map<String, Object> claims = Map.of(
-                TokenClaim.ROLES.getClaim(), ServiceRole.INTERNAL.toString(),
-                Claims.AUDIENCE, audience.toString()
-        );
-
         JwtPayload jwtPayload = JwtPayload.builder()
                 .subject(ServiceName.INDEXER_SERVICE.toString())
-                .claims(claims)
+                .claims(Map.of(TokenClaim.ROLES.getClaim(), ServiceRole.INTERNAL.toString()))
+                .audience(Set.of(ServiceRole.INTERNAL.toString()))
                 .expiration(internal.expirationTime())
                 .build();
 
