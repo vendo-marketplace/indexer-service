9a954c16400fc9b783d551c70e073cbe3cf6f465
HaaaamBurger <gemasclashes@gmail.com>
2026-07-04 10:17:54 +0300

    fixing index initializer, logging

--- src/main/java/com/vendo/indexer_service/adapter/product/out/elasticsearch/index/ProductIndexInitializer.java
+++ src/main/java/com/vendo/indexer_service/adapter/product/out/elasticsearch/index/ProductIndexInitializer.java
@@ -2,23 +2,26 @@ package com.vendo.indexer_service.adapter.product.out.elasticsearch.index;
 
 import com.vendo.indexer_service.adapter.product.out.elasticsearch.ElasticProduct;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.ApplicationRunner;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.core.IndexOperations;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class ProductIndexInitializer {
 
     private final ElasticsearchOperations operations;
 
-    @EventListener(ApplicationRunner.class)
+    @EventListener(ApplicationStartedEvent.class)
     public void init() {
         IndexOperations indexOps = operations.indexOps(ElasticProduct.class);
 
         if (!indexOps.exists()) {
+            log.info("Initializing product index.");
             indexOps.create();
             indexOps.putMapping();
         }
