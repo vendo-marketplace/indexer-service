2446910fc7c9b53fa5e9dcc3122ff1d863609fda
HaaaamBurger <gemasclashes@gmail.com>
2026-07-30 13:28:19 +0300

    kan-97: mapping address fix

--- src/main/java/com/vendo/indexer_service/adapter/product/out/elasticsearch/nested/ElasticAddress.java
+++ src/main/java/com/vendo/indexer_service/adapter/product/out/elasticsearch/nested/ElasticAddress.java
@@ -1,9 +1,11 @@
 package com.vendo.indexer_service.adapter.product.out.elasticsearch.nested;
 
+import lombok.Data;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 import org.springframework.data.elasticsearch.annotations.GeoPointField;
 
+@Data
 public class ElasticAddress {
     @Field(type = FieldType.Keyword)
     private String region;
@@ -13,6 +15,7 @@ public class ElasticAddress {
     @GeoPointField
     private ElasticLocation location;
 
+    @Data
     public static class ElasticLocation {
         private double lat;
         private double lon;
